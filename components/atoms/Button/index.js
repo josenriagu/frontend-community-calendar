1971fed1b5d6dd32c06523d09b08b6116e1a00fc
Martin Twum Mensah <martint.mensah@gmail.com>
2019-12-16 13:19:01 +0000

    chore: configure storybook
    Create a button with a story
    Configure where stories will be loaded from

---
+++ components/atoms/Button/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #4CAF50; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+`;
