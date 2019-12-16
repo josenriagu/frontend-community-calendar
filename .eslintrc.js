57d007187285203d2cd2aacec583f803ff570485
Martin Twum Mensah <martint.mensah@gmail.com>
2019-12-16 15:22:34 +0000

    chore: add eslint
    Install dependencies for eslint
    Add airbnb as a guide for the eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  overrides: [
+    {
+      files: [
+        "**/*.test.js",
+      ]
+    },
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+};
