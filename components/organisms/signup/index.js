eea878bff452681a3e117a17acfa44d4a3905ee3
Akere Adetola <adetolaakere1@gmail.com>
2020-01-07 14:16:52 +0100

    chore(signup): implemented feedback

--- components/organisms/signup/index.js
+++ components/organisms/signup/index.js
@@ -1,4 +1,9 @@
+import React from "react";
 import styled from "styled-components";
+
+import { Button } from "../../atoms/Button";
+import { NavBarAlt } from "../../molecules/Navbar/index.stories";
+import { inputLarge as Input } from "../../atoms/Input/index.stories";
 
 export const BorderDiv = styled.div`
   width: 385px;
@@ -31,3 +36,34 @@ export const Paragraph = styled.p`
   margin-bottom: 35px;
   margin-top: 20px;
 `;
+
+export const Register = () => {
+  return (
+    <div>
+      <NavBarAlt />
+      <BorderDiv>
+        <InputDiv>
+          <Label>Name</Label>
+          <br />
+          <Input type="text" />
+        </InputDiv>
+
+        <InputDiv>
+          <Label>Email address</Label>
+          <br />
+          <Input type="text" />
+        </InputDiv>
+
+        <InputDiv>
+          <Label>Password</Label>
+          <br />
+          <Input type="text" />
+        </InputDiv>
+        <Paragraph>Lagos,NG(Change)</Paragraph>
+        <Button large style={{ width: "358px", height: "52px" }}>
+          Sign up
+        </Button>
+      </BorderDiv>
+    </div>
+  );
+};
--- components/organisms/signup/index.stories.js
+++ components/organisms/signup/index.stories.js
@@ -1,40 +1,7 @@
 import React from "react";
-import { BorderDiv, InputDiv, Label, Paragraph } from "./index";
-import { Button } from "../../atoms/Button";
-import { NavBarAlt } from "../../molecules/Navbar/index.stories";
-import { inputLarge as Input } from "../../atoms/Input/index.stories";
 
-const Register = () => {
-  return (
-    <div>
-      <NavBarAlt />
-      <BorderDiv>
-        <InputDiv>
-          <Label>Name</Label>
-          <br />
-          <Input type="text" />
-        </InputDiv>
-
-        <InputDiv>
-          <Label>Email address</Label>
-          <br />
-          <Input type="text" />
-        </InputDiv>
-
-        <InputDiv>
-          <Label>Password</Label>
-          <br />
-          <Input type="text" />
-        </InputDiv>
-        <Paragraph>Lagos,NG(Change)</Paragraph>
-        <Button large style={{ width: "358px", height: "52px" }}>
-          Sign up
-        </Button>
-      </BorderDiv>
-    </div>
-  );
-};
-
-export const Signup = () => <Register />;
+import { Register } from "./";
 
 export default { title: "Register" };
+
+export const Signup = () => <Register />;
