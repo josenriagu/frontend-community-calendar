12c03ad739bd55670bf3712678a3996cf78ef1d0
Josemaria Nriagu <josemarianriagu25@gmail.com>
2020-01-07 17:04:24 +0100

    chore(frontend): refactor atom and molecules
    Co-authored-by: Oluwatola Oluwafemi Blessing <oluwatolablessing@gmail.com>

--- components/molecules/Searchbar/index.stories.js
+++ components/molecules/Searchbar/index.stories.js
@@ -1,18 +1,9 @@
 import React from 'react';
 
-import { SearchBarDiv } from '.';
-import Input from '../../atoms/Input';
-import { Button } from '../../atoms/Button';
-
-export const SearchBar = () => (
-  <SearchBarDiv>
-    <Input search type="text" placeholder="I want events in" />
-    <div>
-      <Input search type="options" placeholder="Happening on               --v--" />
-      <i className=" fas fa-chevron-down" />
-    </div>
-    <Button medium>Search</Button>
-  </SearchBarDiv>
-);
+import SearchBar from '.';
 
 export default { title: 'Searchbar' };
+
+export const Searchbar = () => (
+  <SearchBar />
+);
