c6ab93785bb48365c67e985b3e936a56f656570c
Josemaria Nriagu <josemarianriagu25@gmail.com>
2020-01-10 16:49:09 +0100

    feat(frontend): implement fetched events

--- components/molecules/EventCard/EventCardMin.js
+++ components/molecules/EventCard/EventCardMin.js
@@ -5,25 +5,31 @@ import Icon from '../../atoms/Icon';
 import { EventCardDiv } from './index.styled';
 import { colors } from '../../~reusables';
 
-const EventCardMin = ({ toggle }) => (
-  <EventCardDiv>
-    <div id="parent1">
-      <div><p>10:00AM</p></div>
-      <div id="sibling1">
-        <h6>Event Title</h6>
-        <p>Some subtitle</p>
-        <p>Virginia, US</p>
+const EventCardMin = ({ toggle, el }) => {
+  // eslint-disable-next-line no-param-reassign
+  el.source = 'EventBrite';
+  return (
+    <EventCardDiv>
+      <div id="parent1">
+        <div><p>{el.eventDate.split('+')[0].split(',')[2]}</p></div>
+        <div id="sibling1">
+          <h6>{el.name.includes('-') ? el.name.split('-')[0] : el.name}</h6>
+          <p>{`Source: ${el.source}`}</p>
+          {el.location && <p>{el.location.split('•')[1]}</p>}
+        </div>
       </div>
-    </div>
-    <span id="toggle">
-      <i onClick={toggle} className=" fas fa-chevron-down" />
-      <Icon type="star" color={colors.primary} />
-    </span>
-  </EventCardDiv>
-);
+      <span id="toggle">
+        <i onClick={toggle} className=" fas fa-chevron-down" />
+        <Icon type="star" color={colors.primary} />
+      </span>
+    </EventCardDiv>
+  );
+};
 
 EventCardMin.propTypes = {
   toggle: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  el: PropTypes.array.isRequired,
 };
 
 export default EventCardMin;
