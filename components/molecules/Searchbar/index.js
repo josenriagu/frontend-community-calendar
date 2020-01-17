c115794ae2ea60dd9050ae0cbca945617381ea5b
theallegrarr <nurudeen.amedu@gmail.com>
2020-01-17 11:26:38 +0100

    feat: make search bar functional

--- components/molecules/Searchbar/index.js
+++ components/molecules/Searchbar/index.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 
 import { SearchBarDiv } from './index.styled';
-import Input from '../../atoms/Input';
+import Interests from '../../atoms/Interests';
 import { Button } from '../../atoms/Button';
 import { colors } from '../../~reusables';
 
-const SearchBar = () => (
-  <SearchBarDiv>
-    <Input search type="text" placeholder="I want events in" />
-    <div>
-      <Input search type="options" placeholder="Happening on          " />
-      <i className=" fas fa-chevron-down" />
-    </div>
-    <Button medium background={colors.primary}>Search</Button>
-  </SearchBarDiv>
-);
+const SearchBar = () => {
+  const [location, setLocation] = useState({ country: '', region: '..select a country first' });
+
+  const selectCountry = (val) => setLocation({ ...location, country: val });
+  const selectRegion = (val) => setLocation({ ...location, region: val });
+
+  return (
+    <>
+      <SearchBarDiv>
+        {/* <Input search type="text" placeholder="I want events in" /> */}
+        <Interests />
+        <div>
+          <CountryDropdown
+            value={location.country}
+            onChange={(val) => selectCountry(val)}
+            style={{
+              border: 'none',
+              fontSize: '15pt',
+              maxWidth: '200px',
+            }}
+          />
+        </div>
+        <div>
+          <RegionDropdown
+            value={location.region}
+            country={location.country}
+            onChange={(val) => selectRegion(val)}
+            style={{
+              border: 'none',
+              display: `${location.country ? 'block' : 'none'}`,
+              fontSize: '15pt',
+              maxWidth: '200px',
+            }}
+          />
+        </div>
+        <Button medium background={colors.primary}>Search</Button>
+      </SearchBarDiv>
+    </>
+  );
+};
 
 export default SearchBar;
